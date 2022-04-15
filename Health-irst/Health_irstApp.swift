142dcc6f73a28b5ac044742a83537de9d2c41a74
dotAlexX <alope616@fiu.edu>
2022-04-15 04:44:25 -0400

    Initial Commit

---
+++ Health-irst/Health_irstApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_irstApp.swift
+//  Health-irst
+//
+//  Created by dotAlexX on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Health_irstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
